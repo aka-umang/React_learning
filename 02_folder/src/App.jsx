f19fe090bca4294b3d34feeeb67f71bf7e1cffc7
genuineUmang <genuineumang@gmail.com>
2026-07-02 02:47:13 +0530

    02_folder

---
+++ 02_folder/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/card"
+import Navbar from "./components/navbar";
+
+function App() {
+  return(
+    <>
+      <Navbar/>
+      <Card/>
+    </>
+  );
+}
+
+export default App;
---
+++ 02_folder/src/components/card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+  return (
+    <div className="card">
+      <h1>Umang Maheshwari</h1>
+      <h2>Aspiring Software engineer</h2>
+    </div>
+  );
+}
+
+export default Card;
